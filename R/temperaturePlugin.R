a3fde51f5b0b8c84ba15d70fcc5a7d2f8f0963a8
Romain Forestier <romain.forestier48@gmail.com>
2020-02-13 12:18:24 +1100

    testing help section for function

--- R/temperaturePlugin.R
+++ R/temperaturePlugin.R
@@ -1,7 +1,7 @@
 #' tempFun is a function that takes temperature parameters (temperature, t_ref, t_d) and physiological parameters (w, Ea, c_a)
 #' and returns a scalar based on the Padfield (2016) temperature performance equation. This will be upgraded in the future with
 #' a deactivation portion with Anna Gardmark's equation once it's published
-tempFun <- function(w, temperature, t_d = 25, t_ref, Ea, c_a) # default are 0 for now as deactivation is buggy
+tempFun <- function(w, temperature, t_ref, Ea, c_a)
 {
   k = 8.617332e-5 # Boltzmann constant 
   # equation
@@ -10,18 +10,37 @@ tempFun <- function(w, temperature, t_d = 25, t_ref, Ea, c_a) # default are 0 fo
   # converting to Kelvin from Celcius
   temperature <- temperature + 273 
   t_ref <- t_ref + 273
-  t_d <- t_d + 273
   
   temperatureScalar <- t(sapply(w,FUN = function(x){x^(c_a*(temperature-(t_ref)))}) *exp((-Ea/k)*((1/temperature) - (1/(t_ref)))))
   return(temperatureScalar)
 }
 
-#' temperatureMizer is a function that uses the latest version of Mizer (02/2020) and changes 4 parameters that are temperature sensitive
+
+#' Temperature Mizer plugin
+#' 
+#' uses the latest version of Mizer (02/2020) and changes 4 parameters that are temperature sensitive
 #' (search volume / max intake / metabolism / background mortality) according to the scalars from tempFun.
 #' At the moment this function only allow time independent changes (i.e temperature is constant during the simulation and the affected parameters
 #' do not vary through time)
 #' this function takes and return a Mizer params object
 #' 
+#' 
+#' @param params A \linkS4class{MizerParams} object
+#' @param t_ref Temperature where scalar equals 1. Default is 10 degrees C.
+#' @param temperature Ambient temperature influencing the scalar. Only an integer at the moment meaning that temperature do not vary
+#' during projections. Default is set to t_ref (i.e. temperature has no effect)
+#' @param ea_int Activation energy relative to food intake. This parameter affects the search volume and intake max
+#' Default is set to 0 (i.e. no effect)
+#' @param ca_int Size dependence activation relative to food intake. Negative values flatten the performance curve slope with increasing size.
+#' Default is set to 0 (i.e. no size dependence)
+#' @param ea_met Activation energy relative to metabolism
+#' @param ca_met Size dependence activation relative to metabolsim
+#' @param ea_mor Activation energy relative to background mortality
+#' @param ca_mor Size dependence activation relative to mortality
+#' 
+#' @return A \linkS4class{MizerParams} object
+#' @export
+
 temperatureMizer <- function(params, t_ref = 10, temperature = t_ref, 
                              ea_int = 0,ca_int = 0, ea_met = 0, ca_met = 0, ea_mor = 0, ca_mor = 0)
 {
